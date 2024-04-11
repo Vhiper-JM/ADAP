184fbff1740bcfd9747bcf7dcb123310ae5d3e5f
Vhiper-JM <jmmosqueram@gmail.com>
2024-04-11 17:35:27 -0500

    Checkpoint Conexion Login Firebase - Autenticacion

--- ADAP/Inicio_sesion/views.py
+++ ADAP/Inicio_sesion/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import pyrebase
 
-# Create your views here.
+app_name = 'Inicio_sesion'
+
+# Conexion FireBase
+firebaseConfig = {
+  "apiKey": "AIzaSyCkeIBWE--pQhFUWgJ0ownE_le1ixzJBxw",
+  "authDomain": "auto-liderazgodb.firebaseapp.com",
+  "databaseURL": "https://auto-liderazgodb-default-rtdb.firebaseio.com",
+  "projectId": "auto-liderazgodb",
+  "storageBucket": "auto-liderazgodb.appspot.com",
+  "messagingSenderId": "815578098109",
+  "appId": "1:815578098109:web:7680bb79cd30766d580ad4",
+   "measurementId": "G-S4GRF8BD9B"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+autenticacion = firebase.auth()
+database = firebase.database()
+
+# Recordar remplazar vew login OFICIAL
+def login(request):
+    return render(request, 'TempLogin.html')
+
+# vew login
+def postlogin(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    try:
+        user = autenticacion.sign_in_with_email_and_password(email, password)
+        return render(request, 'conexEXI.html', {'email': email})
+    except:
+        return render(request, 'TempLogin.html', {'error': 'Credenciales incorrectas'})
