07a86478629e3ce931619287820e923e2206446c
Vhiper-JM <jmmosqueram@gmail.com>
2024-04-12 09:03:38 -0500

    Cambio ConexionDB.py - Arreglo names, ID's etc Company y SignUp views

--- ADAP/Inicio_sesion/ConexionDB.py
+++ ADAP/Inicio_sesion/ConexionDB.py
@@ -1,9 +1,28 @@
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import os  # Import the os module for path operations
 
-#Credenciales de la base de datos
-firebase_sdk = credentials.Certificate('auto-liderazgodb-firebase-adminsdk-jtcig-7a58637b6f.json')
-#Referencia a la base de datos
-firebase_admin.initialize_app(firebase_sdk, {'databaseURL': 'https://auto-liderazgodb-default-rtdb.firebaseio.com/'})
+# Get the current directory where this script is located
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Construct the full path to the JSON file
+json_file_path = os.path.join(current_dir, 'auto-liderazgodb-firebase-adminsdk-jtcig-7a58637b6f.json')
+
+# Check if the JSON file exists
+if os.path.isfile(json_file_path):
+    # Credenciales de la base de datos
+    firebase_sdk = credentials.Certificate(json_file_path)
+    # Referencia a la base de datos
+    firebase_admin.initialize_app(firebase_sdk, {'databaseURL': 'https://auto-liderazgodb-default-rtdb.firebaseio.com/'})
+    # Referencia a la base de datos con nombre de variable descriptivo
+    refDB = db.reference()
+    # Print the contents of the tables in the database in an organized way
+    print("Printing contents of the tables in the database:")
+    for table_name in refDB.get().keys():
+        table_data = refDB.child(table_name).get()
+        print(f"Table: {table_name}")
+        print(json.dumps(table_data, indent=4))  # Format the table data using json.dumps
+else:
+    print(f"Error: JSON file not found at {json_file_path}")
--- ADAP/Inicio_sesion/views.py
+++ ADAP/Inicio_sesion/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+import json
 import pyrebase
+from django.shortcuts import render
+from .ConexionDB import refDB  # Importing db reference from ConexionDB.py
 
 app_name = 'Inicio_sesion'
 
-# Conexion FireBase
-firebaseConfig = {
+# Pyrebase initialization for user authentication
+config = {
   "apiKey": "AIzaSyCkeIBWE--pQhFUWgJ0ownE_le1ixzJBxw",
   "authDomain": "auto-liderazgodb.firebaseapp.com",
   "databaseURL": "https://auto-liderazgodb-default-rtdb.firebaseio.com",
@@ -12,18 +14,17 @@ firebaseConfig = {
   "storageBucket": "auto-liderazgodb.appspot.com",
   "messagingSenderId": "815578098109",
   "appId": "1:815578098109:web:7680bb79cd30766d580ad4",
-   "measurementId": "G-S4GRF8BD9B"
+  "measurementId": "G-S4GRF8BD9B"
 }
 
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(config)
 autenticacion = firebase.auth()
-database = firebase.database()
 
 # Recordar remplazar vew login OFICIAL
 def login(request):
     return render(request, 'TempLogin.html')
 
-# vew login
+# view login
 def postlogin(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
@@ -39,3 +40,108 @@ def signin(request):
 
 def signin_business(request):
     return render(request, 'Company_SignUp.html')
+
+# view create user
+def create_user(request):
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+    idUser = request.POST.get('id')
+    gender = request.POST.get('gender')
+    nationality = request.POST.get('nationality')
+    phone = request.POST.get('phone')
+    country = request.POST.get('country')
+    date = request.POST.get('date')
+    email = request.POST.get('email')
+    company = request.POST.get('company')
+    position = request.POST.get('position')
+    entrepreneur = request.POST.get('entrepreneur')
+    entrepreneurship = request.POST.get('entrepreneurship')
+
+    # Create a dictionary with user data
+    user_data = {
+        'name': name,
+        'surname': surname,
+        'idUser': idUser,
+        'gender': gender,
+        'nationality': nationality,
+        'phone': phone,
+        'country': country,
+        'date': date,
+        'email': email,
+        'company': company,
+        'position': position,
+        'entrepreneur': entrepreneur,
+        'entrepreneurship': entrepreneurship
+    }
+
+
+    try:
+        print(user_data)
+        # Reference to the Firebase Realtime Database
+        print(refDB)
+        
+        # Retrieve user data from Firebase
+        user_data_dict = refDB.child('Usuario').get()  # Retrieve JSON data as a string
+        print(user_data_dict)
+        
+        email_exists = False
+        # Check if the email already exists in users data
+        for key, data in user_data_dict.items():  # Loop through dictionary items correctly
+            # Imprimir key y data
+            print(f'Key: {key}, Data: {data}')
+            if key == 'email' and data == email:
+                # Si el email ya existe, imprmir la siguiente linea
+                print(f'Email ya existe: {email}')
+                email_exists = True
+                break 
+
+        if email_exists:
+            return render(request, 'TempLogin.html', {'error': 'Usuario ya existe'})
+        else:
+            refDB.child('Usuario').push(user_data)
+            return render(request, 'TempLogin.html', {'error': 'Registro exitoso'})
+
+    except Exception as e:
+        # Log or handle exceptions more specifically
+        return render(request, 'TempLogin.html', {'error': f'Error: {e}'})
+
+
+def create_user2(request):
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+    idUser = request.POST.get('id')
+    gender = request.POST.get('gender')
+    nationality = request.POST.get('nationality')
+    phone = request.POST.get('phone')
+    country = request.POST.get('country')
+    date = request.POST.get('date')
+    email = request.POST.get('email')
+    company = request.POST.get('company')
+    position = request.POST.get('position')
+    entrepreneur = request.POST.get('entrepreneur')
+    entrepreneurship = request.POST.get('entrepreneurship')
+
+    # Create a dictionary with user data
+    user_data = {
+        'name': name,
+        'surname': surname,
+        'idUser': idUser,
+        'gender': gender,
+        'nationality': nationality,
+        'phone': phone,
+        'country': country,
+        'date': date,
+        'email': email,
+        'company': company,
+        'position': position,
+        'entrepreneur': entrepreneur,
+        'entrepreneurship': entrepreneurship
+    }
+
+    try:
+        # Reference to the Firebase Realtime Database
+        users_ref = refDB.reference('Usuario')
+        users_ref.push(user_data)  # Use set() instead of push() if you want to overwrite existing data
+        return render(request, 'TempLogin.html', {'error': 'Registro exitoso'})  # Redirect to a success page
+    except Exception as e:
+        return render(request, 'TempLogin.html', {'error': f'Error: {e}'})  # Handle any errors during the operation
