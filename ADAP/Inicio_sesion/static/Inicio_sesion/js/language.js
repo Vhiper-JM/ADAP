acb31f00910ceaf8145c90e742472b38a914c4de
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-04-20 15:54:09 -0500

    Empezando funcionamiento de cambio de idioma desde JS

---
+++ ADAP/Inicio_sesion/static/Inicio_sesion/js/language.js
@@ -0,0 +1,17 @@
+const flags = document.getElementById('flags');
+const textsToChange = document.querySelectorAll("[data-section]")
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`../../../static/Inicio_sesion/languages/${language}.json`);
+    const texts = await requestJson.json()
+    console.log(texts)
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML=texts[section][value];
+    }
+} 
+
+flags.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language)
+})
