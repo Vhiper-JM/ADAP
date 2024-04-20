30a689ca6599d9d7498164604976f528b2a3ed56
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-04-20 15:55:22 -0500

    Ajustadas url para el cambio de idioma con JS

--- ADAP/Inicio_sesion/urls.py
+++ ADAP/Inicio_sesion/urls.py
@@ -5,10 +5,10 @@ app_name = 'Inicio_sesion'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:lang>/login', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('postlogin/', views.postlogin, name='postlogin'),
-    path('<str:lang>/signup/', views.signup, name='signup'),
-    path('<str:lang>/signup_business/', views.signup_business, name='signup_business'),
+    path('signup/', views.signup, name='signup'),
+    path('signup_business/', views.signup_business, name='signup_business'),
     path('register_user/', views.register_user, name='register_user'),
     path('register_company/', views.register_company, name='register_company'),
 
