919bb8adb84d52e3508516f17646c072d39fe2e1
Vhiper-JM <jmmosqueram@gmail.com>
2024-04-22 14:35:02 -0500

    Vista Home Empresa - Personal Data Check - Pending Formularios

--- ADAP/Formulario/views.py
+++ ADAP/Formulario/views.py
@@ -25,3 +25,55 @@ def userView(request):
             return HttpResponse('Error al obtener información del usuario')
     else:
         return HttpResponse('Correo no proporcionado en la sesión')
+
+def companyView(request):
+    # Retrieve email from session
+    email = request.session.get('user_email')
+    if email:
+        # Assuming you have a function to fetch user information based on email from ConexionDB module
+        company_info = CDB.get_user_info(email)
+        if company_info:
+            # Pass the user information to the template for rendering
+            return render(request, 'Formulario/en/ViewCompany.html', {'company_info': company_info})
+        else:
+            return HttpResponse('Error al obtener información del usuario')
+    else:
+        return HttpResponse('Correo no proporcionado en la sesión')
+    
+
+    
+def desempContextual(request):
+    """
+    View for desempenocontextual.html
+    """
+    return render(request, 'Formulario/desempenocontextual.html')
+
+def desempContraproducente(request):
+    """
+    View for desempenocontraproducente.html
+    """
+    return render(request, 'Formulario/desempenocontraproducente.html')
+
+def desempTareas(request):
+    """
+    View for desempenodetareas.html
+    """
+    return render(request, 'Formulario/desempenodetareas.html')
+
+def estraConstructivas(request):
+    """
+    View for estrategiasconstructivas.html
+    """
+    return render(request, 'Formulario/estrategiasconstructivas.html')
+
+def estraComportamiento(request):
+    """
+    View for estrategiasdecomportamiento.html
+    """
+    return render(request, 'Formulario/estrategiasdecomportamiento.html')
+
+def estraRecompensa(request):
+    """
+    View for estrategiasderecompensa.html
+    """
+    return render(request, 'Formulario/estrategiasderecompensa.html')
